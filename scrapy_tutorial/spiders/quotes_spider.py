6a19923f48ab997de3314a2cc729f65b379d52d7
Felipe Barros <felipe.b4rros@gmail.com>
2022-02-04 10:00:16 -0300

    using response.follow_all

--- scrapy_tutorial/spiders/quotes_spider.py
+++ scrapy_tutorial/spiders/quotes_spider.py
@@ -14,6 +14,6 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('div.tags a.tag::text').getall(),
             }
-        next_page = response.css('li.next a::attr(href)').get()
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+        anchors = response.css('ul.pager a')
+        yield from response.follow_all(anchors, callback=self.parse)
+
